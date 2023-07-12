aa2784d75c08e7af89f61d38dccd862e6a7de56a
Cameron Liu <emailforschool123123@gmail.com>
2023-07-13 00:46:44 +0800

    Bug fixes and added social icons

--- components/footer.js
+++ components/footer.js
@@ -7,16 +7,31 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer>
-        <p class="copyright">Copyright ©2023 Hell's University
-            <br><br>
-            Massachusetts Hall, Cambridge, MA 02138, USA
-            <br>
-            (702) 731-7373
-            <br>
-            <br>
-            DISCLAIMER: Hell's University shall not be liable for any loss or damage caused by the usage of any
-            information obtained from this site or representatives.
-        </p>
+            <div class="row">
+                <p class="copyright">Copyright ©2023 Hell's University
+                    <br><br>
+                    Massachusetts Hall, Cambridge, MA 02138, USA
+                    <br>
+                    (702) 731-7373
+                    <br>
+                    <br>
+                    DISCLAIMER: Hell's University shall not be liable for any loss or damage caused by the usage of any
+                    information obtained from this site or representatives.
+                </p>
+                    <div class="social-icon">
+                    <a href="https://www.facebook.com/gordonramsay" target="_blank"> 
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    
+                    <a href="https://www.instagram.com/gordongram/" target="_blank"> 
+                        <i class="fab fa-instagram"></i>
+                    </a>
+
+                    <a href="https://www.youtube.com/watch?v=VZrDxD0Za9I" target="_blank"> 
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
         </footer>
         `;
     }
